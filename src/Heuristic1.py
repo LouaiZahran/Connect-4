78cfe10a58711529db86719c0614b543e0ccec1a
Gamal Abdul Hameed Nasef Newysar <es-gamalabdelhameed314@alexu.edu.eg>
2022-11-08 18:03:43 +0200

    Move Heuristic to Heuristic-1... To Be Continued

--- src/Heuristic1.py
+++ src/Heuristic1.py
@@ -1,6 +1,189 @@
 from . import Heuristic
+from . import State
+import math
 
 
 class Heuristic1(Heuristic):
-    pass
+    __max_value = math.pow(10, 12)
 
+    def get_score(self, current_state: State):
+        board = current_state.get_board()
+        result: int = self.__get_single_chips(board) + self.__get_two_chips(board) + \
+                      self.__get_three_chips(board) + self.__get_three_chips(board)
+        return result
+
+    def __get_single_chips(self, board: list) -> int:
+        result: int = 0
+        for j in range(7):
+            for i in range(6):
+                if board[i][j] == 0:
+                    break
+                result += self.__calc_single_chip(board[i][j], i, j)
+        return result
+
+    def __calc_single_chip(self, player: int, i: int, j: int) -> int:
+        if j == 0 or j == 6:
+            if i == 0 or i == 5:
+                value = 24
+            elif i == 1 or i == 4:
+                value = 32
+            else:
+                value = 40
+        elif j == 1 or j == 5:
+            if i == 0 or i == 5:
+                value = 35
+            elif i == 1 or i == 4:
+                value = 53
+            else:
+                value = 70
+        elif j == 2 or j == 4:
+            if i == 0 or i == 5:
+                value = 55
+            elif i == 1 or i == 4:
+                value = 87
+            else:
+                value = 120
+        else:
+            if i == 0 or i == 5:
+                value = 108
+            elif i == 1 or i == 4:
+                value = 154
+            else:
+                value = 200
+        if player == 1:
+            return -value
+        return value
+
+    def __get_two_chips(self, board: list) -> int:
+        result: int = 0
+        for i in range(6):
+            for j in range(6):
+                if board[i][j] == 0:
+                    continue
+                if board[i][j] == board[i][j + 1]:
+                    empty_spaces = 0
+                    k = j - 1
+                    while k >= 0 and board[i][k] == 0:
+                        empty_spaces += 1
+                        k -= 1
+                    k = j + 2
+                    while k < 7 and board[i][k] == 0:
+                        empty_spaces += 1
+                        k += 1
+                    result += self.__calc_two_chips(board[i][j], empty_spaces)
+        for j in range(7):
+            for i in range(3):
+                if board[i][j] == 0:
+                    break
+                if board[i][j] == board[i + 1][j]:
+                    empty_spaces = 0
+                    for k in range(i + 2, 6):
+                        if board[k][j] != 0:
+                            break
+                        empty_spaces += 1
+                    result += self.__calc_two_chips(board[i][j], empty_spaces)
+        for i in range(5):
+            for j in range(6):
+                if board[i][j] == 0:
+                    break
+                if board[i][j] == board[i + 1][j + 1]:
+                    r, c = i - 1, j - 1
+                    empty_spaces = 0
+                    while r >= 0 and c >= 0 and board[r][c] == 0:
+                        empty_spaces += 1
+                        r -= 1
+                        c -= 1
+                    r, c = i + 2, j + 2
+                    while r < 6 and c < 7 and board[r][c] == 0:
+                        empty_spaces += 1
+                        r += 1
+                        c += 1
+
+                    result += self.__calc_two_chips(board[i][j], empty_spaces)
+            for j in range(6, 0, -1):
+                if board[i][j] == 0:
+                    break
+                if board[i][j] == board[i + 1][j - 1]:
+                    r, c = i - 1, j + 1
+                    empty_spaces = 0
+                    while r >= 0 and c < 6 and board[r][c] == 0:
+                        empty_spaces += 1
+                        r -= 1
+                        c += 1
+                    r, c = i + 2, j - 2
+                    while r < 6 and c >= 0 and board[r][c] == 0:
+                        empty_spaces += 1
+                        r += 1
+                        c -= 1
+                    result += self.__calc_two_chips(board[i][j], empty_spaces)
+        return result
+
+    def __calc_two_chips(self, player: int, empty_spaces: int):
+        result: int = 0
+        if empty_spaces > 1:
+            if player == 1:
+                result -= (empty_spaces - 1) * 10000
+            else:
+                result += (empty_spaces - 1) * 10000
+        return result
+
+    def __get_three_chips(self, board: list):
+        return 0  # to be implemented
+
+    def __get_four_chips(self, board: list):
+        return 0  # to be implemented
+
+    def __calc_six_chips(self, board: list) -> int:
+        result = 0
+        for i in range(6):
+            flag = True
+            for j in range(6):
+                if board[i][j] != 2:
+                    flag = False
+                    break
+            if flag:
+                result += 3 * self.__max_value
+                if board[i][6] == 2:
+                    result += 900000
+            flag = True
+            for j in range(6):
+                if board[i][j] != 1:
+                    flag = False
+                    break
+            if flag:
+                result -= 3 * self.__max_value
+                if board[i][6] == 1:
+                    result -= 900000
+            flag = True
+            for j in range(1, 7):
+                if board[i][j] != 2:
+                    flag = False
+            if flag:
+                result += 3 * self.__max_value
+                if board[i][0] == 2:
+                    result += 900000
+            flag = True
+            for j in range(1, 7):
+                if board[i][j] != 1:
+                    flag = False
+            if flag:
+                result -= 3 * self.__max_value
+                if board[i][0] == 1:
+                    result -= 900000
+        for j in range(7):
+            flag = True
+            for i in range(6):
+                if board[i][j] != 2:
+                    flag = False
+                    break
+            if flag:
+                result += 3 * self.__max_value
+            flag = True
+            for i in range(6):
+                if board[i][j] != 1:
+                    flag = False
+                    break
+            if flag:
+                result -= 3 * self.__max_value
+        # Diagonal
+        return result
