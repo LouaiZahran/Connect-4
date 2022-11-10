da98740285a8a329db77c3638f2bbe0e82028fad
Gamal Abdul Hameed Nasef Newysar <es-gamalabdelhameed314@alexu.edu.eg>
2022-11-10 17:59:44 +0200

    Started Heuristic-1
    Added Heuristic 1 for rows only, need to check. If correct, I will complete it for columns and diagonals.

--- src/Heuristic2.py
+++ src/Heuristic2.py
@@ -1,5 +1,47 @@
 from . import Heuristic
+from . import State
 
 
 class Heuristic2(Heuristic):
-    pass
+    def get_score(self, current_state: State):
+        board = current_state.get_board()
+        result: int = 0
+        for j in range(7):
+            for i in range(6):
+                if board[i][j] == 0:
+                    break
+                result += self.__calc_single_chip(board[i][j], i, j)
+        return result
+
+    def __calc_single_chip(self, player: int, i: int, j: int) -> int:
+        if j == 0 or j == 6:
+            if i == 0 or i == 5:
+                value = 3
+            elif i == 1 or i == 4:
+                value = 4
+            else:
+                value = 5
+        elif j == 1 or j == 5:
+            if i == 0 or i == 5:
+                value = 4
+            elif i == 1 or i == 4:
+                value = 6
+            else:
+                value = 8
+        elif j == 2 or j == 4:
+            if i == 0 or i == 5:
+                value = 5
+            elif i == 1 or i == 4:
+                value = 8
+            else:
+                value = 11
+        else:
+            if i == 0 or i == 5:
+                value = 7
+            elif i == 1 or i == 4:
+                value = 10
+            else:
+                value = 13
+        if player == 1:
+            return -value
+        return value
